45ebfb863e1c6e6ce28a08a3800f11bf069ad2f3
lasterbadges <81156192+lasterbadges@users.noreply.github.com>
2026-07-03 16:13:53 +0500

    2
    Signed-off-by: lasterbadges <81156192+lasterbadges@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -110,3 +110,43 @@ def classify_ore_segments(filled_mask, image_enhanced_gray, brightness_threshold
     final_mask[talc_condition] = 3
 
     return final_mask
+
+    def slicer(image_path, output_dir):
+
+        # 1. Укажите путь к вашей картинке с синими контурами
+        image_path = image_path
+        image = cv2.imread(image_path)
+
+        # Выходная папка
+        output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
+
+        if image is None:
+            print("Ошибка: Картинка не найдена!")
+        else:
+            # Высота и ширина картинки
+            height, width, _ = image.shape
+
+            # Размер одного слайса
+            tile_size = 512
+
+            # Колво файлов
+            count = 0
+
+            # Задаем список кординат по верт заранее чтобы избежать выхода за границу
+            y_cords = list(range(0, height, tile_size))
+            if y_cords[-1] + tile_size > height:
+                y_cords[-1] = height - tile_size
+
+            # Задаем список кординат по гориз заранее чтобы избежать выхода за границу
+            x_cords = list(range(0, width, tile_size))
+            if x_cords[-1] + tile_size > width:
+                x_cords[-1] = width - tile_size
+
+            for y in y_cords:
+                for x in x_cords:
+                    # 2. Вырезаем кусочек: от 0 до 512 пикселей по высоте и по ширине
+                    crop = image[y:y + tile_size, x:x + tile_size]
+                    # 3. Сохраняем этот кусочек рядом с кодом
+                    cv2.imwrite(f"{output_dir}/crop_{count}.jpg", crop)
+                    count += 1
